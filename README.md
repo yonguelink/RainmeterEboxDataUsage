0a84b1f902db91fd4853b44efade305ec7924773
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2017-01-14 13:37:25 -0500

    Initial commit working with the current (new) ebox client website

---
+++ README.md
@@ -0,0 +1,9 @@
+# EBox Data Usage Rainmeter
+
+This is a simple Rainmeter block to see your EBox data usage.
+
+## How to use
+
+Simply rename the file `config.json.example` in the `bin` folder to `config.js` and configure it with your client code and client password.
+
+Then load the skin via Rainmeter. It'll automatically refresh every 30 minutes, you can also force it to refresh by right clicking on it and hitting "Refresh".
---
+++ bin/js/eboxUsage.js
@@ -0,0 +1,65 @@
+//Only info that cannot be placed in the config file
+var configFile = "bin/config.json";
+
+//Requires
+var fs = require('fs');
+var isFirst = false;
+fs.write("allo", fs.workingDirectory, "w");
+
+//Settings
+var settings = loadConfigs(configFile);
+
+function loadConfigs(path){
+	var data = fs.read(path);
+	while(data.indexOf("	") > -1)data = data.replace("	", "");
+	return JSON.parse(data);
+};
+
+
+var page = require('webpage').create();
+
+page.onLoadStarted = function() {
+	canExit = false;
+};
+
+page.onLoadFinished = function(){
+	if(!isFirst){
+		isFirst = true;
+	}else{
+		var text = evaluateOtherPage();
+		end(text);
+	}
+};
+
+page.open(settings.url, function(status) {
+  if(status === "success") {
+	  page.includeJs(settings.jQueryUrl, function() {
+		  page.evaluate(function(settings){
+			  var formdata = document.formLogin;
+			  formdata.usrname.value = settings.clientCode;
+			  formdata.pwd.value = settings.clientPass;
+			  formdata.submit();
+		  }, settings);
+	  });
+  }
+  
+});
+
+function evaluateOtherPage(){
+	var text = page.evaluate(function(){
+		var unCleanText = $(".text_summary3").text().trim().replace("\n", " ");
+		while(unCleanText.indexOf("\t") > -1)unCleanText = unCleanText.replace("\t", "");
+		while(unCleanText.indexOf("o") > -1)unCleanText = unCleanText.replace("o", "");
+		unCleanText = unCleanText.replace(" / ", "/");
+		return unCleanText;
+	});
+	//mendatory as output
+	console.log(text);
+	return text;
+}
+
+function end(text){
+	var path = settings.outputFile;
+	fs.write(path, text, 'w');
+	phantom.exit();
+}
